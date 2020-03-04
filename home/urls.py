7d079a984873688ff027bdb99de4dd91098d8942
BestNico <Lzmkxumgh@gmail.com>
2020-03-04 17:54:05 +0900

    [2020/03/04] integration fronted

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,6 @@ app_name = 'home'
 
 urlpatterns = [
     path('', views.home_page, name='home_page'),
+    path('contact', views.contact, name='contact'),
+    path('about', views.about, name='about'),
 ]
--- home/views.py
+++ home/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, 'index_test.html')
+    return render(request, 'index.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def about(request):
+    return render(request, 'about.html')

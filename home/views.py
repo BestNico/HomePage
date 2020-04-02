3a2f443f5f39155af2f4d833eec12f6493d402de
BestNico <Lzmkxumgh@gmail.com>
2020-04-02 17:57:39 +0900

    [2020/04/02] Add blog detail page

--- home/views.py
+++ home/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
+from article.models import Article
+import markdown
 
 
 def home_page(request):
-    return render(request, 'homepage.html')
+    blog_list = Article.objects.all()
+    context = {'blog_list': blog_list}
+    return render(request, 'homepage.html', context)
+
+def home_blog_detail(request, id):
+    blog = Article.objects.get(id=id)
+    blog.body = markdown.markdown(blog.body,extensions=[
+                                'markdown.extensions.extra',
+                                'markdown.extensions.codehilite',
+                                'markdown.extensions.toc',
+                            ])
+    context = {'blog':blog}
+    return render(request, 'homepage_detail.html', context)
 
 def index(request):
     return render(request, 'index.html')
